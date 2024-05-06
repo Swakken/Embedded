a5077f5a2396a6bb42c1a1003cb5cdd70b7fe53f
Swakken <76128423+Swakken@users.noreply.github.com>
2024-05-06 17:34:17 +0200

    Update display.c

--- Nim/lib/display/display.c
+++ Nim/lib/display/display.c
@@ -3,9 +3,18 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+#define SPACE 0xFF;
+
 /* Segment byte maps for numbers 0 to 9 */
 const uint8_t SEGMENT_MAP[] = {0xC0, 0xF9, 0xA4, 0xB0, 0x99,
                                0x92, 0x82, 0xF8, 0X80, 0X90};
+
+                    
+/* Byte map to select letters a to z*/
+const uint8_t ALPHABET_MAP[] = {0x88, 0x83, 0xC6, 0xA1, 0x86, 0x8E, 0xC2,
+                                0x89, 0xCF, 0xE1, 0x8A, 0xC7, 0xEA, 0xC8,
+                                0xC0, 0x8C, 0x4A, 0xCC, 0x92, 0x87, 0xC1,
+                                0xC1, 0xD5, 0x89, 0x91, 0xA4};
 
 /* Byte maps to select digit 1 to 4 */
 const uint8_t SEGMENT_SELECT[] = {0xF1, 0xF2, 0xF4, 0xF8};
@@ -70,4 +79,36 @@ void writeNumberAndWait(int number, int delay) {
     writeNumberToSegment(3, number % 10);
     _delay_ms(5);
   }
+}
+
+//Schrijft cijfer naar bepaald segment. Segment 0 is meest linkse.
+void writeCharToSegment(uint8_t segment, char character) {
+
+  // verander kleine letters naar grote letters
+  if (character >= 'a' && character <= 'z' ){
+    character = character - 32;
+  }
+  uint8_t value;
+  // Is het karakter een hoofdletter?
+  if ( character < 'A' && character > 'Z')
+  {
+    value = SPACE;
+  } else
+  {
+    value = ALPHABET_MAP[character - 'A'];
+  }
+
+  cbi(PORTD, LATCH_DIO);
+  shift(value, MSBFIRST);
+  shift(SEGMENT_SELECT[segment], MSBFIRST);
+  sbi(PORTD, LATCH_DIO);
+}
+
+//Schrijft getal tussen 0 en 9999 naar de display. Te gebruiken in een lus...
+void writeString(char* str) {
+  for (int i = 0; i < 4; i++)
+  {
+    writeCharToSegment(i, str[i]);
+  }
+
 }

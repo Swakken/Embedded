be432f63c5955d02995ee73cd86445b9868acb87
Swakken <76128423+Swakken@users.noreply.github.com>
2024-05-06 16:21:54 +0200

    Update display.c

--- Nim/lib/display/display.c
+++ Nim/lib/display/display.c
@@ -3,21 +3,12 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define SPACE 0xff
-
 /* Segment byte maps for numbers 0 to 9 */
 const uint8_t SEGMENT_MAP[] = {0xC0, 0xF9, 0xA4, 0xB0, 0x99,
                                0x92, 0x82, 0xF8, 0X80, 0X90};
 
 /* Byte maps to select digit 1 to 4 */
 const uint8_t SEGMENT_SELECT[] = {0xF1, 0xF2, 0xF4, 0xF8};
-
-/* Byte map voor letters*/
-const uint8_t ALPHABET_MAP[] = {0x88, 0x83, 0xC6, 0xA1, 0x86, 0x8E, 0xC2,
-                                0x89, 0xCF, 0xE1, 0x8A, 0xC7, 0xEA, 0xC8,
-                                0xC0, 0x8C, 0x4A, 0xCC, 0x92, 0x87, 0xC1,
-                                0xC1, 0xD5, 0x89, 0x91, 0xA4};
-
 
 void initDisplay() {
   sbi(DDRD, LATCH_DIO);
@@ -51,32 +42,10 @@ void shift(uint8_t val, uint8_t bitorder) {
 //Schrijft cijfer naar bepaald segment. Segment 0 is meest linkse.
 void writeNumberToSegment(uint8_t segment, uint8_t value) {
   cbi(PORTD, LATCH_DIO);
-  shift(ALPHABET_MAP[value], MSBFIRST);
+  shift(SEGMENT_MAP[value], MSBFIRST);
   shift(SEGMENT_SELECT[segment], MSBFIRST);
   sbi(PORTD, LATCH_DIO);
 }
-
-/*TEST*/
-void writeCharToSegment(uint8_t segment, char character) {
-    uint8_t value;
-
-    // Verander kleine letter naar grote letter
-    if (character >= 'a' && character <= 'z') {
-        character -= 32;  // Verander kleine letter naar grote letter
-    }
-    
-    if (character < 'A' || character > 'Z') {
-        value = SPACE;
-    } else {
-        value = ALPHABET_MAP[character - 'A'];
-    } 
-
-    cbi(PORTD, LATCH_DIO);
-    shift(value, MSBFIRST);
-    shift(SEGMENT_SELECT[segment], MSBFIRST);
-    sbi(PORTD, LATCH_DIO);
-}
-
 
 //Schrijft getal tussen 0 en 9999 naar de display. Te gebruiken in een lus...
 void writeNumber(int number) {
@@ -85,13 +54,6 @@ void writeNumber(int number) {
   writeNumberToSegment(1, (number / 100) % 10);
   writeNumberToSegment(2, (number / 10) % 10);
   writeNumberToSegment(3, number % 10);
-}
-
-void writeString(char* str) {
-  for (int i = 0; i < 4; i++)
-  {
-    writeCharToSegment(i, str[i]);
-  }
 }
 
 //Schrijft getal tussen 0 en 9999 naar de display en zorgt dat het er een bepaald aantal milliseconden blijft staan.

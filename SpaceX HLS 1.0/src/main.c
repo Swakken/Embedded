3ee3139930c6b15d8001e1f315d87960044c1c55
Swakken <76128423+Swakken@users.noreply.github.com>
2024-05-15 10:03:08 +0200

    Update main.c

---
+++ SpaceX HLS 1.0/src/main.c
@@ -0,0 +1,34 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <usart.h>
+#include <pot.h>
+#include "display.h"
+#include <button.h>
+
+#define LED1 0
+#define LED2 1
+#define LED3 2
+#define LED4 3
+
+#define BUTTON_PIN PCINT1
+#define BUTTON_PIN2 PCINT2
+#define BUTTON_PIN3 PCINT3
+
+int main() {
+    initUSART();
+    startPotentiometer();
+    initDisplay(); 
+
+    
+
+    while (1) {
+        writeNumber(9999);
+        
+
+    }
+
+    return 0;
+
+}

23079687a593e67beb43abd865e60a570b18ffe8
Swakken <76128423+Swakken@users.noreply.github.com>
2024-05-19 16:03:53 +0200

    Update main.c

--- Fappy Bird/src/main.c
+++ Fappy Bird/src/main.c
@@ -20,81 +20,46 @@
 #define LED3 2
 #define LED4 3
 
-
+#define BUZZER_PIN PB1
 #define DELAY_TIME 200
 #define NUM_LEDS 4
 
 
-int gameStarted = 0;
-int lightShowControl = 1;
+void displayLightShow() {
+    for (uint8_t i = 0; i < 4; i++) {
+        lightUpLed(i);
+        _delay_ms(200);
+        lightDownLed(i);
+        _delay_ms(200);
 
-// Functie om te controleren of een van de knoppen is ingedrukt
-int isAnyButtonPressed() {
-    return (buttonPushed(BUTTON_PIN0) || buttonPushed(BUTTON_PIN1) || buttonPushed(BUTTON_PIN2));
-}
-
-// Wacht totdat een van de knoppen wordt ingedrukt
-void waitForButtonPress() {
-    while (!isAnyButtonPressed()) {
-
-    }
-}
-
-void lightShow() {
-    while (lightShowControl == 1) {
-        for (int i = 0; i < NUM_LEDS; i++) {
-            lightUpLed(i);
-            _delay_ms(500);
-            lightDownLed(i);
-
-            if (lightShowControl != 1) break;
+        if (buttonPushed(PC1) || buttonPushed(PC2) || buttonPushed(PC3)) {
+            return 1;
         }
     }
+    lightUpAllLeds();
+    _delay_ms(500);
+    lightDownAllLeds();
+
+    return 0;
 }
 
-void startSpel() {
-    int gameStarted = 0;
-    printf("Druk op een willekeurige knop om het spel te starten\n");
 
-    while (gameStarted == 0) {
-        lightShowControl = 1;
-        lightShow();
-
-        if (isAnyButtonPressed()) {
-            lightShowControl = 0;
-            gameStarted = 1;
-            printf("Het spel is gestart!\n");
-        }
-    }
-}
-
-// Functie om de gebruiker de snelheid van de obstakels te laten kiezen
-void spelerNiveau() {
-    printf("Draai aan de potentiometer\n");
-
-    int level = 0;
-
-    while (!isAnyButtonPressed()) {
-        level = (readPotentiometer() * 10) / 1024;
-        writeNumber(level);
-    }
-
-    waitForButtonPress();
-
-    gameStarted = 1;
-}
-
-int main() {
+int main(void) {
     initUSART();
-    initDisplay(); 
-
+    enableAllButtons();
+    initDisplay();
     enableAllLeds();
     lightDownAllLeds();
     enableAllButtons();
 
-    startSpel();
+    while (1) {
+        displayLightShow();
 
-    spelerNiveau();
+        if (buttonPushed(PC1) || buttonPushed(PC2) || buttonPushed(PC3)) {
+            printf("Het spel is gestart\n");
+            break;
+        }
+    }
 
     return 0;
 }

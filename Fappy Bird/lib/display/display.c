7ef976216087a0fe21a26fdf8599893f85a88246
Swakken <76128423+Swakken@users.noreply.github.com>
2024-05-20 16:40:06 +0200

    Update display.c
    LINE_MAP

--- Fappy Bird/lib/display/display.c
+++ Fappy Bird/lib/display/display.c
@@ -7,10 +7,9 @@
 
 /* Segment byte maps for numbers 0 to 9 */
 const uint8_t SEGMENT_MAP[] = {0xC0, 0xF9, 0xA4, 0xB0, 0x99,
-                               0x92, 0x82, 0xF8, 0X80, 0X90};
+                               0x92, 0x82, 0xF8, 0x80, 0x90};
 
-                    
-/* Byte map to select letters a to z*/
+/* Byte map to select letters a to z */
 const uint8_t ALPHABET_MAP[] = {0x88, 0x83, 0xC6, 0xA1, 0x86, 0x8E, 0xC2,
                                 0x89, 0xCF, 0xE1, 0x8A, 0xC7, 0xEA, 0xC8,
                                 0xC0, 0x8C, 0x4A, 0xCC, 0x92, 0x87, 0xC1,
@@ -19,18 +18,20 @@ const uint8_t ALPHABET_MAP[] = {0x88, 0x83, 0xC6, 0xA1, 0x86, 0x8E, 0xC2,
 /* Byte maps to select digit 1 to 4 */
 const uint8_t SEGMENT_SELECT[] = {0xF1, 0xF2, 0xF4, 0xF8};
 
+/* Byte maps to select specific line segments */
+const uint8_t LINE_MAP[] = {0xFE, 0xFD, 0xFB, 0xF7, 0xEF, 0xDF, 0xBF};
+
 void initDisplay() {
   sbi(DDRD, LATCH_DIO);
   sbi(DDRD, CLK_DIO);
   sbi(DDRB, DATA_DIO);
 }
 
-// loop through seven segments of LED display and shift the correct bits in the
-// data register
+// Loop through seven segments of LED display and shift the correct bits in the data register
 void shift(uint8_t val, uint8_t bitorder) {
   uint8_t bit;
 
-  for (uint8_t i = 0; i < NUMBER_OF_SEGMENTS; i++) {
+  for (uint8_t i = 0; i < 8; i++) {
     if (bitorder == LSBFIRST) {
       bit = !!(val & (1 << i));
     } else {
@@ -48,7 +49,7 @@ void shift(uint8_t val, uint8_t bitorder) {
   }
 }
 
-//Schrijft cijfer naar bepaald segment. Segment 0 is meest linkse.
+// Schrijft cijfer naar bepaald segment. Segment 0 is meest linkse.
 void writeNumberToSegment(uint8_t segment, uint8_t value) {
   cbi(PORTD, LATCH_DIO);
   shift(SEGMENT_MAP[value], MSBFIRST);
@@ -56,7 +57,7 @@ void writeNumberToSegment(uint8_t segment, uint8_t value) {
   sbi(PORTD, LATCH_DIO);
 }
 
-//Schrijft getal tussen 0 en 9999 naar de display. Te gebruiken in een lus...
+// Schrijft getal tussen 0 en 9999 naar de display. Te gebruiken in een lus...
 void writeNumber(int number) {
   if (number < 0 || number > 9999) return;
   writeNumberToSegment(0, number / 1000);
@@ -65,8 +66,8 @@ void writeNumber(int number) {
   writeNumberToSegment(3, number % 10);
 }
 
-//Schrijft getal tussen 0 en 9999 naar de display en zorgt dat het er een bepaald aantal milliseconden blijft staan.
-//Opgelet: de timing is "ongeveer", er wordt geen rekening gehouden met de tijd writeNumberToSegment in beslag neemt...
+// Schrijft getal tussen 0 en 9999 naar de display en zorgt dat het er een bepaald aantal milliseconden blijft staan.
+// Opgelet: de timing is "ongeveer", er wordt geen rekening gehouden met de tijd writeNumberToSegment in beslag neemt...
 void writeNumberAndWait(int number, int delay) {
   if (number < 0 || number > 9999) return;
   for (int i = 0; i < delay / 20; i++) {
@@ -81,20 +82,17 @@ void writeNumberAndWait(int number, int delay) {
   }
 }
 
-//Schrijft cijfer naar bepaald segment. Segment 0 is meest linkse.
+// Schrijft cijfer naar bepaald segment. Segment 0 is meest linkse.
 void writeCharToSegment(uint8_t segment, char character) {
-
   // verander kleine letters naar grote letters
-  if (character >= 'a' && character <= 'z' ){
+  if (character >= 'a' && character <= 'z') {
     character = character - 32;
   }
   uint8_t value;
   // Is het karakter een hoofdletter?
-  if ( character < 'A' && character > 'Z')
-  {
+  if (character < 'A' || character > 'Z') {
     value = SPACE;
-  } else
-  {
+  } else {
     value = ALPHABET_MAP[character - 'A'];
   }
 
@@ -104,11 +102,17 @@ void writeCharToSegment(uint8_t segment, char character) {
   sbi(PORTD, LATCH_DIO);
 }
 
-//Schrijft getal tussen 0 en 9999 naar de display. Te gebruiken in een lus...
+// Schrijft getal tussen 0 en 9999 naar de display. Te gebruiken in een lus...
 void writeString(char* str) {
-  for (int i = 0; i < 4; i++)
-  {
+  for (int i = 0; i < 4; i++) {
     writeCharToSegment(i, str[i]);
   }
+}
 
+// Teken een lijn (segment) op het LED display
+void drawLine(uint8_t segment, uint8_t value) {
+  cbi(PORTD, LATCH_DIO);
+  shift(LINE_MAP[value], MSBFIRST);
+  shift(SEGMENT_SELECT[segment], MSBFIRST);
+  sbi(PORTD, LATCH_DIO);
 }

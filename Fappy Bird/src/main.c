e3d1f2252d8b9549e97bef05beb9ce8662c0c5ff
Swakken <76128423+Swakken@users.noreply.github.com>
2024-05-25 11:09:34 +0200

    Update main.c

--- Fappy Bird/src/main.c
+++ Fappy Bird/src/main.c
@@ -16,36 +16,22 @@
 #define BUTTON_PIN1 PC1  
 #define BUTTON_PIN2 PC2
 
-#define LED1 0
-#define LED2 1
-#define LED3 2
-#define LED4 3
-
 #define BUZZER_PIN PB1
 #define DELAY_TIME 200
 #define NUM_LEDS 4
 
 int level = 1;
 
-
-int displayLightShow() {
-    for (uint8_t i = 0; i < 4; i++) {
-        lightUpLed(i);
-        _delay_ms(200);
-        lightDownLed(i);
-        _delay_ms(200);
-
-        if (buttonPushed(PC1) || buttonPushed(PC2) || buttonPushed(PC3)) {
-            lightDownAllLeds();
-            return 1;
-        }
-    }
-    lightUpAllLeds();
-    _delay_ms(500);
-    lightDownAllLeds();
-
-    return 0;
+void displayLightShow(){
+  printf("Druk op een willekeurige button om het spel te starten.\n");
+  
+  while (!(buttonPushed(PC1) || buttonPushed(PC2) || buttonPushed(PC3))) {
+    drawLine(rand() % 4, rand() % 7);
+    _delay_ms(100);
+  }
+  _delay_ms(500);
 }
+
 
 
 // Ik moet echt 2 rondjes draaien aan de potentiometer om 1 getal hoger of lager te krijgen? 
@@ -165,27 +151,8 @@ int main(void) {
     startPotentiometer();
     initDisplay();
 
-    int buttonPressed = 0;
+    displayLightShow();
 
-
-    printf("Druk op een willekeurige button om het spel te starten.\n");
-
-    while (1) {
-        if (!buttonPressed) {
-            displayLightShow();
-        }
-
-        if (!buttonPressed && (buttonPushed(PC1) || buttonPushed(PC2) || buttonPushed(PC3))) {
-            lightDownAllLeds();
-            printf("Het spel is gestart\n");
-            buttonPressed = 1;
-            _delay_ms(100);
-        } else if (buttonPressed == 1 && (buttonPushed(PC1) || buttonPushed(PC2) || buttonPushed(PC3))) {
-            int level = kiesLevel();
-            printf("Starten met niveau: %d\n", level);
-            break;
-        }
-    }
     
     printf("Het spel begint nu!\n");
     printf("Gebruik button 1 om de flappy bird langs de opstakels te laten vliegen\n");
@@ -195,7 +162,7 @@ int main(void) {
 
         if (!paused) {
             obstakels();
-            flappyBird();
+            // flappyBird();
 
         } else {
             clearDisplay();

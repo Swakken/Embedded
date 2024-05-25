547e60bfa4ec4faaba32f470bdac3e84beb20c3c
Swakken <76128423+Swakken@users.noreply.github.com>
2024-05-25 17:32:41 +0200

    Update pot.c

--- Fappy Bird/lib/potentio/pot.c
+++ Fappy Bird/lib/potentio/pot.c
@@ -2,15 +2,13 @@
 #include <stdint.h>
 
 
-void startPotentiometer()
-{
+void startPotentiometer() {
     ADMUX |= (1<<REFS0); //instellen van de reference voltage. We kiezen op deze manier voor de 5V als reference voltage
     ADCSRA |= (1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);//bepalen van de samplerate door een deelfactor in te stellen. Deelfactor hier: 128
     ADCSRA |= (1<<ADEN);//Enable de ADC
 }
 
-uint16_t readPotentiometer()
-{
+uint16_t readPotentiometer() {
     ADCSRA |= (1<<ADSC);//start de conversie analoog -> digitaal
     loop_until_bit_is_clear(ADCSRA,ADSC);//wacht tot conversie gebeurd is
     uint16_t value = ADC;//lees het resultaat uit
